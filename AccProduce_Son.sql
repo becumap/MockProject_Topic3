151346ff2edae8be3656af30478a4be211a157c4
becumap <hongson2331994@gmail.com>
2017-04-08 07:49:34 +0700

    Account produce
    Fixed and tested account produce

--- AccProduce_Son.sql
+++ AccProduce_Son.sql
@@ -19,38 +19,30 @@ CREATE PROC InsertAcc
 @CreateDate date,
 @Role nvarchar(50),
 @Status bit
-
 )
 AS
 BEGIN
-	INSERT INTO Account(UseName) VALUES(@UseName)
-	INSERT INTO Account(Password) VALUES(@Password)
-	INSERT INTO Account(FullName) VALUES(@FullName)
-	INSERT INTO Account(SDT) VALUES(@SDT)
-	INSERT INTO Account(CreateDate) VALUES(@CreateDate)
-	INSERT INTO Account(Role) VALUES(@Role)
-	INSERT INTO Account(Status) VALUES(@Status)
+	INSERT INTO Account(UseName,Password,FullName,SDT,CreateDate,Role,Status) VALUES(@UseName,@Password,@FullName,@SDT,@CreateDate,@Role,@Status)
+	
 END
 
+EXEC InsertAcc 'son1','123','son nguyen',789,NULL,'Admin',1
 --update account
 CREATE PROC UpdateAcc
 (
 @UseName varchar(15),
 @FullName nvarchar(60),
 @SDT int,
-@CreateDate date,
 @Role nvarchar(50),
 @Status bit
-
 )
 AS
 BEGIN
-	UPDATE Account SET FullName=@FullName WHERE UseName LIKE @UseName
-	UPDATE Account SET SDT=@SDT WHERE UseName LIKE @UseName
-	UPDATE Account SET CreateDate=@CreateDate WHERE UseName LIKE @UseName
-	UPDATE Account SET Role=@Role WHERE UseName LIKE @UseName
-	UPDATE Account SET Status=@Status WHERE UseName LIKE @UseName
+	UPDATE Account SET FullName=@FullName,SDT =@SDT, Role =@Role,Status = @Status	
+	 WHERE UseName LIKE @UseName	
 END
+
+EXEC UpdateAcc 'son1','Nguyen Hong Son',56456154,'Member',0
 
 CREATE PROC DeleteAcc
 (
