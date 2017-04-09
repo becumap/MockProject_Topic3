114db68d53dc4fbe595b8fbdf2c488c2cc5d0f32
becumap <hongson2331994@gmail.com>
2017-04-09 08:10:34 +0700

    Produce
    Fiexed and add produce

--- AccProduce_Son.sql
+++ AccProduce_Son.sql
@@ -1,5 +1,6 @@
 USE [SIMS]
 --GET LIST ACCOUNT
+--Nho kiem tra no co ton tai hay khong.
 CREATE PROC GETLISTACC
 AS
 BEGIN 
@@ -10,7 +11,7 @@ END
 EXEC GETLISTACC
 
 --ADD ACCOUNT
-CREATE PROC InsertAcc
+alter PROC InsertAcc
 (
 @UseName varchar(15),
 @Password varchar(15),
@@ -23,12 +24,17 @@ CREATE PROC InsertAcc
 AS
 BEGIN
 	INSERT INTO Account(UseName,Password,FullName,SDT,CreateDate,Role,Status) VALUES(@UseName,@Password,@FullName,@SDT,@CreateDate,@Role,@Status)
-	
+	if @@ERROR <>0
+		return 0
+	else
+		return 1
 END
 
-EXEC InsertAcc 'son1','123','son nguyen',789,NULL,'Admin',1
+declare @tmp int;
+EXEC @tmp = InsertAcc 'son10','123','son nguyen',789,NULL,'Admin',1
+select @tmp
 --update account
-CREATE PROC UpdateAcc
+Alter PROC UpdateAcc
 (
 @UseName varchar(15),
 @FullName nvarchar(60),
@@ -39,19 +45,29 @@ CREATE PROC UpdateAcc
 AS
 BEGIN
 	UPDATE Account SET FullName=@FullName,SDT =@SDT, Role =@Role,Status = @Status	
-	 WHERE UseName LIKE @UseName	
+	 WHERE UseName LIKE @UseName
+	 if @@ERROR <>0
+		return 0
+	else
+		return 1
 END
+declare @tmp int;
+EXEC @tmp = UpdateAcc 'son10','Nguyen Hong Son',56456154,'Member',0
+select @tmp
 
-EXEC UpdateAcc 'son1','Nguyen Hong Son',56456154,'Member',0
-
-CREATE PROC DeleteAcc
+Alter PROC DeleteAcc
 (
 	@UseName varchar(15)
 )
 AS
 BEGIN
 	DELETE FROM Account WHERE UseName LIKE  @UseName
+	if @@ERROR <>0
+		return 0
+	else
+		return 1
 END
-
-EXEC DeleteAcc 'son1'
+declare @tmp int;
+EXEC @tmp= DeleteAcc 'son10'
+select @tmp
 
